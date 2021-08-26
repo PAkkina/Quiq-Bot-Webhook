922bcbb0daa11fdf3d985c6d5f53406b6b52321a
Shariq Shahzad <shariq_shahzad07@yahoo.com>
2021-08-26 03:27:53 -0700

    return command added

--- controllers/webHookController.js
+++ controllers/webHookController.js
@@ -5,6 +5,7 @@ const quiqResponseParser = require('../utils/quiq-response-parser')
 exports.getOrderDetailByOrderNumberAndpostalCode = async (req, res, next) => {
     const postalCode = req.body.conversation.custom.zipCode;
     const orderNumber = req.body.conversation.custom.orderNumber;
+    res.statusCode = 200;
     try {
         let response = await axios.get(`${constants.ORDER_DETAILS_API_URL}`, {
             params: {
@@ -13,15 +14,14 @@ exports.getOrderDetailByOrderNumberAndpostalCode = async (req, res, next) => {
             },
         });
         if (response.status == 403) {
-            res.statusCode = 200;
+            console.log(`UnAuthorized`)
             res.json({ actions: quiqResponseParser.createOrderDetailActions() });
+            return;
         }
-        res.statusCode = 200;
         let responseObject = { actions: quiqResponseParser.createOrderDetailActions(response.data.orderDetailBean.orderData, postalCode, orderNumber), waitForCustomerResponseOverride: { shouldWait: false } }
         res.json(responseObject);
     }
     catch (e) {
-        res.statusCode = 200;
         res.json({ actions: quiqResponseParser.createOrderDetailActions() });
         console.log(e);
     }
