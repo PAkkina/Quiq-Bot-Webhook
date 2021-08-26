8d15469bc03d101a06daa3c4b97694482805739a
Shariq Shahzad <shariq_shahzad07@yahoo.com>
2021-08-25 23:58:45 -0700

    sample api added to test heroku

--- controllers/webHookController.js
+++ controllers/webHookController.js
@@ -6,21 +6,23 @@ exports.getOrderDetailByOrderNumberAndpostalCode = async (req, res, next) => {
     const postalCode = req.body.conversation.custom.zipCode;
     const orderNumber = req.body.conversation.custom.orderNumber;
     try {
-        let response = await axios.get(`${constants.ORDER_DETAILS_API_URL}`, {
-            params: {
-                postalCode,
-                orderNumber
-            },
-            headers: {
-                'Accept-Encoding': 'gzip, deflate, br',
-                'Accept': '*/*',
-                'Content-Type': 'application/json',
-                'Content-Length' : 7
-            },
-        });
+        let response = await axios.get(`https://jsonplaceholder.typicode.com/todos`);
+        // let response = await axios.get(`${constants.ORDER_DETAILS_API_URL}`, {
+        //     params: {
+        //         postalCode,
+        //         orderNumber
+        //     },
+        //     headers: {
+        //         'Accept-Encoding': 'gzip, deflate, br',
+        //         'Accept': '*/*',
+        //         'Content-Type': 'application/json',
+        //         'Content-Length' : 7
+        //     },
+        // });
         res.statusCode = 200;
-        let responseObject = { actions: quiqResponseParser.createOrderDetailActions(response.data.orderDetailBean.orderData, postalCode, orderNumber), waitForCustomerResponseOverride: { shouldWait: false } }
-        res.json(responseObject);
+        res.json(response.data);
+        // let responseObject = { actions: quiqResponseParser.createOrderDetailActions(response.data.orderDetailBean.orderData, postalCode, orderNumber), waitForCustomerResponseOverride: { shouldWait: false } }
+        // res.json(responseObject);
     }
     catch (e) {
         res.statusCode = 500;
