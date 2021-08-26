b5daf0e0e3f15af49ca82b777d79acc1145073bb
Shariq Shahzad <shariq_shahzad07@yahoo.com>
2021-08-25 23:40:36 -0700

    axios method change

--- controllers/webHookController.js
+++ controllers/webHookController.js
@@ -6,19 +6,22 @@ exports.getOrderDetailByOrderNumberAndpostalCode = async (req, res, next) => {
     const postalCode = req.body.conversation.custom.zipCode;
     const orderNumber = req.body.conversation.custom.orderNumber;
     try {
-        let response = await axios({
-            method: 'get',
-            headers: {
-                'Accept-Encoding': 'gzip, deflate, br',
-                'Accept':'*/*',
-                'Content-Type':'application/json'
-            },
-            url: `${constants.ORDER_DETAILS_API_URL}`,
+        let response = await axios.get(`${constants.ORDER_DETAILS_API_URL}`,{
             params: {
                 postalCode,
                 orderNumber
             }
-        });
+        })
+        // let response = await axios({
+        //     method: 'get',
+        //     headers: {
+        //         'Accept-Encoding': 'gzip, deflate, br',
+        //         'Accept': '*/*',
+        //         'Content-Type': 'application/json'
+        //     },
+        //     url: `${constants.ORDER_DETAILS_API_URL}`,
+
+        // });
         res.statusCode = 200;
         let responseObject = { actions: quiqResponseParser.createOrderDetailActions(response.data.orderDetailBean.orderData, postalCode, orderNumber), waitForCustomerResponseOverride: { shouldWait: false } }
         res.json(responseObject);
