c1f6d03c26e0639fb0ad4463bbe249723afff8a8
shariqshahzad <shariq_shahzad07@yahoo.com>
2021-09-30 03:21:10 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-
 var indexRouter = require('./routes/index');
 var webHookRouter = require('./routes/webHookRouter');
+var logger = require('morgan');
 
 var app = express();
 
