bab84c7b58c4e5dbd6217d933a06885f3ec27c59
Shariq Shahzad <shariq_shahzad07@yahoo.com>
2021-10-06 07:07:13 -0700

    static credentials implementation

--- app.js
+++ app.js
@@ -11,7 +11,30 @@ var app = express();
 // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'jade');
+app.use((req, res, next) => {
 
+  // -----------------------------------------------------------------------
+  // authentication middleware
+
+  const auth = { login: 'pbqaenv', password: 'Bl@ck2ye' } // change this
+
+  // parse login and password from headers
+  const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
+  const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':')
+
+  // Verify login and password are set and correct
+  if (login && password && login === auth.login && password === auth.password) {
+    // Access granted...
+    return next()
+  }
+
+  // Access denied...
+  res.set('WWW-Authenticate', 'Basic realm="401"') // change this
+  res.status(401).send('Authentication required.') // custom message
+
+  // -----------------------------------------------------------------------
+
+})
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
